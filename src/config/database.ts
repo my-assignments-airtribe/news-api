5bd2e5f930ed0e988ae08345aba62451d7376339
Ashrith Prakash <ashrith@lostinkaos.com>
2023-10-03 13:51:14 +0530

    folder stuctures added

---
+++ src/config/database.ts
@@ -0,0 +1,25 @@
+// Import any necessary libraries/modules here.
+import mongoose, { ConnectOptions } from 'mongoose';
+
+// MongoDB connection URL
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/news_api';
+
+// MongoDB options (optional)
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+} as ConnectOptions;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, mongoOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+export default mongoose.connection;
