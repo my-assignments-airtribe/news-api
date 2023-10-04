686d66664a9b03ea886ee8761a4b74f976695032
Ashrith Prakash <ashrith@lostinkaos.com>
2023-10-04 12:31:50 +0530

    first commit

--- src/routes/newsRoutes.ts
+++ src/routes/newsRoutes.ts
@@ -8,4 +8,36 @@ const router = express.Router();
 
 router.get('/articles', authenticateJWT, getNewsArticles);
 
+router.post(":id/read", (req, res) => {
+  // get the id of the article
+  // need to update the user model to store read article ids
+  // post the id to the user's read articles
+  // return the updated user model
+})
+
+router.get("/read", (req, res) => {
+  // get the user's read articles
+  // return the user's read articles
+});
+
+router.post(":id/favorite", (req, res) => {
+  // get the id of the article
+  // need to update the user model to store favorite article ids
+  // post the id to the user's favorite articles
+  // return the updated user model
+})
+
+router.get("/favorites", (req, res) => {
+  // get the user's favorite articles
+  // return the user's favorite articles
+});
+
+router.get('/search/:keyword', (req, res) => {
+  // get the keyword from the params
+  // search the news api for the keyword
+  // return the articles
+})
+
+// Figure out how to cache the news articles and upate articles in the backround
+
 export default router;
