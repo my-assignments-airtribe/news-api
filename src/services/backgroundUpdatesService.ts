3ab364edfbd2b1934a1ed6b33b5064eddb41bfa7
Ashrith Prakash <ashrith@lostinkaos.com>
2023-10-05 19:08:02 +0530

    minor updates

--- src/services/backgroundUpdatesService.ts
+++ src/services/backgroundUpdatesService.ts
@@ -4,7 +4,8 @@ import { fetchNews } from './fetchNewsService';
 import { setCache } from './cacheServie';
 
 export const startBackgroundUpdates = () => {
-  cron.schedule('0 0 * * *', async () => {
+  // Run every 5 minutes
+  cron.schedule('*/5 * * * *', async () => {
     try {
       const users = await User.find().select('preferences');
 
@@ -23,4 +24,26 @@ export const startBackgroundUpdates = () => {
       console.error('Error updating cached news articles:', error);
     }
   });
+
+  // setInterval(async () => {
+  //   try {
+  //     const users = await User.find().select('preferences');
+
+  //     for (const user of users) {
+  //       const cacheKey = 'news-' + user._id;
+
+  //       // Fetch news data from an external API (e.g., News API)
+  //       const articles = await fetchNews(user);
+
+  //       // Update the cache with fresh news articles
+  //       setCache(cacheKey, articles, 3600); // Cache for 1 hour
+  //     }
+
+  //     console.log('Cached news articles updated.');
+  //   } catch (error) {
+  //     console.error('Error updating cached news articles:', error);
+  //   }
+  // }
+  // , 300000); // 5 minutes
+
 };
